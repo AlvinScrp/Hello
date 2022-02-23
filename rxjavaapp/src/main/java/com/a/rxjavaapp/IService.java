ff15d32a6db14ba5389aefe2eb6b4cc459ca9664
AlvinScrp <351033166@qq.com>
2022-02-23 22:53:54 +0800

    提交retrofit+okhttp修改

--- rxjavaapp/src/main/java/com/a/rxjavaapp/IService.java
+++ rxjavaapp/src/main/java/com/a/rxjavaapp/IService.java
@@ -1,9 +1,19 @@
 package com.a.rxjavaapp;
 
-import io.reactivex.Observable;
-import retrofit2.http.GET;
+import java.util.List;
 
+import io.reactivex.Single;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+//定义一个接口类
 public interface IService {
-    @GET("/banner/json")
-    Observable<BannerResponse> banner();
+    //通过注解定义我们的网络请求方法。返回 retrofit2.call
+    @GET("users/{user}")
+    Call<UserBean> userInfo(@Path("user") String user);
+
+    //返回 RxJava  Single
+    @GET("users/{user}")
+    Single<UserBean> userInfoRx(@Path("user") String user);
 }
--- rxjavaapp/src/main/java/com/a/rxjavaapp/RxJavaAppActivity.kt
+++ rxjavaapp/src/main/java/com/a/rxjavaapp/RxJavaAppActivity.kt
@@ -1,38 +1,81 @@
 package com.a.rxjavaapp
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_rx_java_app.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 
 class RxJavaAppActivity : AppCompatActivity() {
 
-   private var service = Retrofit.Builder()
-            .baseUrl("https://www.wanandroid.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .build().create(IService::class.java)
+    private var userName: String = "AlvinScrp"
+
+    private var service = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build().create(IService::class.java)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx_java_app)
 
-        btn.setOnClickListener {
-            service.banner()
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe {
-                        tv.text = Gson().toJson(it.data[0])
-                    }
-        }
+        btnOkHttp.setOnClickListener { okhttp() }
+        btnRetrofit.setOnClickListener { retrofit() }
+        btnRetrofitAndRxJava.setOnClickListener { retrofitAndRxJava() }
+
+
+    }
+
+    private fun okhttp() {
+        val request = Request.Builder()
+            .url("https://api.github.com/users/$userName").get().build()
+        val okHttpClient = OkHttpClient()
+        var call = okHttpClient.newCall(request)
+        call.enqueue(object : okhttp3.Callback {
+            override fun onFailure(call: okhttp3.Call, e: IOException) {}
+            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+                if (response.code == 200) {
+                    var responseString = response.body?.string();
+                    var user = Gson().fromJson(responseString, UserBean::class.java)
+                    Handler(Looper.getMainLooper()).post { tv.text = "OkHttp: \n ${user.url}" }
+                }
+            }
+        })
+    }
+
+    private fun retrofit() {
+        service.userInfo(userName).enqueue(object : Callback<UserBean> {
+            override fun onFailure(call: Call<UserBean>, t: Throwable) {}
+            override fun onResponse(call: Call<UserBean>, response: Response<UserBean>) {
+                tv.text = "retrofit: \n ${response.body()?.url}"
+            }
+        })
+
+    }
+
+    private fun retrofitAndRxJava() {
+        service.userInfoRx(userName)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(Consumer { tv.text = "retrofitAndRxJava: \n ${it.url }"  })
 
     }
 }
+
